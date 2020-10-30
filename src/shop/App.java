c147037459363a3901aad05488490069503d017e
pupil <pupil@ivkhk.ee>
2020-10-30 10:35:19 +0200

    Создан класс App, больше ничего нет.

---
+++ src/shop/App.java
@@ -0,0 +1,14 @@
+package shop;
+
+public class App {
+    public static enum storageFiles{GOODS, CUSTOMERS, USERS, STORIES};
+    
+    public App(){
+    }
+
+    public void run() {
+        System.out.println("Магазин продуктов");
+        
+    }
+    
+}
